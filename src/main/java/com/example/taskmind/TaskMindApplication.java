1b1e171a88bdd048c54e5bfa76c27cee66d8b301
SagarKandel1 <93831966+SagarKandel1@users.noreply.github.com>
2025-08-09 13:29:06 +0100

    Initial commit

---
+++ src/main/java/com/example/taskmind/TaskMindApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMindApplication.class, args);
+	}
+
+}
